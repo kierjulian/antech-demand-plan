b67fc98074022824ec3730d3fe18a5e98e01a8f9
Kier Julian Genoguin <kmgenoguin@up.edu.ph>
2019-10-22 22:10:51 +0800

    Update demand plan page to display details of each product sales.
    Signed-off-by: Kier Julian Genoguin <kmgenoguin@up.edu.ph>

--- src/main/java/ph/edu/up/antech/controller/view/DemandPlanController.java
+++ src/main/java/ph/edu/up/antech/controller/view/DemandPlanController.java
@@ -1,12 +1,19 @@
 package ph.edu.up.antech.controller.view;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
+import ph.edu.up.antech.domain.ProductSales;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/demand-plan")
@@ -21,7 +28,15 @@ public class DemandPlanController {
 	private String port;
 
 	@GetMapping("")
-	public String loadDemandPlanPage() {
+	public String loadDemandPlanPage(Model model) {
+		String apiPath = getProductSalesApiPath();
+		ResponseEntity<List<ProductSales>> responseEntity =
+				restTemplate.exchange(apiPath, HttpMethod.GET, null,
+						new ParameterizedTypeReference<List<ProductSales>>() {
+						});
+
+		List<ProductSales> productSalesList = responseEntity.getBody();
+		model.addAttribute("productSalesList", productSalesList);
 		return "demand-plan";
 	}
 
