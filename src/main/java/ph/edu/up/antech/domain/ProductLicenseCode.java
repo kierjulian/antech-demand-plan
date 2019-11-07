edd7f07ba36e238aa22b82851961466353791dd6
Kier Julian Genoguin <kmgenoguin@up.edu.ph>
2019-11-07 11:11:22 +0800

    Further updated UI and accepted values for some input fields.
    Signed-off-by: Kier Julian Genoguin <kmgenoguin@up.edu.ph>

--- src/main/java/ph/edu/up/antech/domain/ProductLicenseCode.java
+++ src/main/java/ph/edu/up/antech/domain/ProductLicenseCode.java
@@ -2,6 +2,17 @@ package ph.edu.up.antech.domain;
 
 public enum ProductLicenseCode {
 
-	TYPE_3_2ND_LICENSE, TYPE_4_REGULATED, WITH_PDEA, WITHOUT_PDEA
+	TYPE_3_2ND_LICENSE("Type 3: 2nd License"), TYPE_4_REGULATED ("Type 4: Regulated"),
+	WITH_PDEA("With PDEA"), WITHOUT_PDEA("Without PDEA");
+
+	private String description;
+
+	private ProductLicenseCode(String description) {
+		this.description = description;
+	}
+
+	public String getDescription() {
+		return description;
+	}
 
 }
