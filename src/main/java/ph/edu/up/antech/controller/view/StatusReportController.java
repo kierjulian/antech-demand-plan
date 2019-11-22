0d8f2e57a49b347be3a942d4803bb3227ced93d5
Kier Julian Genoguin <kmgenoguin@up.edu.ph>
2019-11-22 22:25:11 +0800

    Added page for status reports.
    Signed-off-by: Kier Julian Genoguin <kmgenoguin@up.edu.ph>

---
+++ src/main/java/ph/edu/up/antech/controller/view/StatusReportController.java
@@ -0,0 +1,16 @@
+package ph.edu.up.antech.controller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/reports")
+public class StatusReportController {
+
+    @GetMapping("")
+    public String loadStatusReportHomePage() {
+        return "status-report";
+    }
+
+}
