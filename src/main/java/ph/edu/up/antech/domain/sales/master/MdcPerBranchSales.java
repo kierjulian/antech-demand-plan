85b4efec8850ffd88db47623fb0e8c059b9ec870
Kier Julian Genoguin <kmgenoguin@up.edu.ph>
2019-11-26 10:56:37 +0800

    Added remaining domain classes for master files.
    Signed-off-by: Kier Julian Genoguin <kmgenoguin@up.edu.ph>

---
+++ src/main/java/ph/edu/up/antech/domain/sales/master/MdcPerBranchSales.java
@@ -0,0 +1,534 @@
+package ph.edu.up.antech.domain.sales.master;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Year;
+
+public class MdcPerBranchSales {
+
+    private LocalDate date;
+    private String cono;
+    private String rec;
+    private String bran;
+    private String satbrn;
+    private String customerNo;
+    private String shpcn;
+    private String customerName;
+    private String cadd1;
+    private String cadd2;
+    private String clazz;
+    private String sman;
+    private String prin;
+    private String subpr;
+    private String refcd;
+    private String refcd1;
+    private String netQuantity;
+    private BigDecimal netValue;
+    private BigDecimal netValue2;
+    private String sku;
+    private String category;
+    private BigDecimal grossValue;
+    private Month month;
+    private Year year;
+    private LocalDate referenceDate;
+    private String referenceNo;
+    private String xReferenceNo;
+    private String reasn;
+    private String prodcd;
+    private Integer quantityQr;
+    private Integer quantitySh;
+    private String um;
+    private BigDecimal vlamt;
+    private BigDecimal sellpr;
+    private BigDecimal pds;
+    private LocalDate expirationDate;
+    private String lotNo;
+    private String barcode;
+    private String pdcode;
+    private String dman;
+    private BigDecimal findsc;
+    private String framt;
+    private String slsyr;
+    private String slsmo;
+    private String slswk;
+    private String appNum;
+    private String poNum;
+    private String guartran;
+    private BigDecimal netSales;
+    private String debtorCode;
+    private String strCode;
+    private String coverage;
+    private String reason;
+    private String branchNo;
+    private String naName;
+    private String dsmName;
+    private String coordinator;
+    private String region;
+
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getCono() {
+        return cono;
+    }
+
+    public void setCono(String cono) {
+        this.cono = cono;
+    }
+
+    public String getRec() {
+        return rec;
+    }
+
+    public void setRec(String rec) {
+        this.rec = rec;
+    }
+
+    public String getBran() {
+        return bran;
+    }
+
+    public void setBran(String bran) {
+        this.bran = bran;
+    }
+
+    public String getSatbrn() {
+        return satbrn;
+    }
+
+    public void setSatbrn(String satbrn) {
+        this.satbrn = satbrn;
+    }
+
+    public String getCustomerNo() {
+        return customerNo;
+    }
+
+    public void setCustomerNo(String customerNo) {
+        this.customerNo = customerNo;
+    }
+
+    public String getShpcn() {
+        return shpcn;
+    }
+
+    public void setShpcn(String shpcn) {
+        this.shpcn = shpcn;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCadd1() {
+        return cadd1;
+    }
+
+    public void setCadd1(String cadd1) {
+        this.cadd1 = cadd1;
+    }
+
+    public String getCadd2() {
+        return cadd2;
+    }
+
+    public void setCadd2(String cadd2) {
+        this.cadd2 = cadd2;
+    }
+
+    public String getClazz() {
+        return clazz;
+    }
+
+    public void setClazz(String clazz) {
+        this.clazz = clazz;
+    }
+
+    public String getSman() {
+        return sman;
+    }
+
+    public void setSman(String sman) {
+        this.sman = sman;
+    }
+
+    public String getPrin() {
+        return prin;
+    }
+
+    public void setPrin(String prin) {
+        this.prin = prin;
+    }
+
+    public String getSubpr() {
+        return subpr;
+    }
+
+    public void setSubpr(String subpr) {
+        this.subpr = subpr;
+    }
+
+    public String getRefcd() {
+        return refcd;
+    }
+
+    public void setRefcd(String refcd) {
+        this.refcd = refcd;
+    }
+
+    public String getRefcd1() {
+        return refcd1;
+    }
+
+    public void setRefcd1(String refcd1) {
+        this.refcd1 = refcd1;
+    }
+
+    public String getNetQuantity() {
+        return netQuantity;
+    }
+
+    public void setNetQuantity(String netQuantity) {
+        this.netQuantity = netQuantity;
+    }
+
+    public BigDecimal getNetValue() {
+        return netValue;
+    }
+
+    public void setNetValue(BigDecimal netValue) {
+        this.netValue = netValue;
+    }
+
+    public BigDecimal getNetValue2() {
+        return netValue2;
+    }
+
+    public void setNetValue2(BigDecimal netValue2) {
+        this.netValue2 = netValue2;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public BigDecimal getGrossValue() {
+        return grossValue;
+    }
+
+    public void setGrossValue(BigDecimal grossValue) {
+        this.grossValue = grossValue;
+    }
+
+    public Month getMonth() {
+        return month;
+    }
+
+    public void setMonth(Month month) {
+        this.month = month;
+    }
+
+    public Year getYear() {
+        return year;
+    }
+
+    public void setYear(Year year) {
+        this.year = year;
+    }
+
+    public LocalDate getReferenceDate() {
+        return referenceDate;
+    }
+
+    public void setReferenceDate(LocalDate referenceDate) {
+        this.referenceDate = referenceDate;
+    }
+
+    public String getReferenceNo() {
+        return referenceNo;
+    }
+
+    public void setReferenceNo(String referenceNo) {
+        this.referenceNo = referenceNo;
+    }
+
+    public String getxReferenceNo() {
+        return xReferenceNo;
+    }
+
+    public void setxReferenceNo(String xReferenceNo) {
+        this.xReferenceNo = xReferenceNo;
+    }
+
+    public String getReasn() {
+        return reasn;
+    }
+
+    public void setReasn(String reasn) {
+        this.reasn = reasn;
+    }
+
+    public String getProdcd() {
+        return prodcd;
+    }
+
+    public void setProdcd(String prodcd) {
+        this.prodcd = prodcd;
+    }
+
+    public Integer getQuantityQr() {
+        return quantityQr;
+    }
+
+    public void setQuantityQr(Integer quantityQr) {
+        this.quantityQr = quantityQr;
+    }
+
+    public Integer getQuantitySh() {
+        return quantitySh;
+    }
+
+    public void setQuantitySh(Integer quantitySh) {
+        this.quantitySh = quantitySh;
+    }
+
+    public String getUm() {
+        return um;
+    }
+
+    public void setUm(String um) {
+        this.um = um;
+    }
+
+    public BigDecimal getVlamt() {
+        return vlamt;
+    }
+
+    public void setVlamt(BigDecimal vlamt) {
+        this.vlamt = vlamt;
+    }
+
+    public BigDecimal getSellpr() {
+        return sellpr;
+    }
+
+    public void setSellpr(BigDecimal sellpr) {
+        this.sellpr = sellpr;
+    }
+
+    public BigDecimal getPds() {
+        return pds;
+    }
+
+    public void setPds(BigDecimal pds) {
+        this.pds = pds;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String getLotNo() {
+        return lotNo;
+    }
+
+    public void setLotNo(String lotNo) {
+        this.lotNo = lotNo;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public String getPdcode() {
+        return pdcode;
+    }
+
+    public void setPdcode(String pdcode) {
+        this.pdcode = pdcode;
+    }
+
+    public String getDman() {
+        return dman;
+    }
+
+    public void setDman(String dman) {
+        this.dman = dman;
+    }
+
+    public BigDecimal getFindsc() {
+        return findsc;
+    }
+
+    public void setFindsc(BigDecimal findsc) {
+        this.findsc = findsc;
+    }
+
+    public String getFramt() {
+        return framt;
+    }
+
+    public void setFramt(String framt) {
+        this.framt = framt;
+    }
+
+    public String getSlsyr() {
+        return slsyr;
+    }
+
+    public void setSlsyr(String slsyr) {
+        this.slsyr = slsyr;
+    }
+
+    public String getSlsmo() {
+        return slsmo;
+    }
+
+    public void setSlsmo(String slsmo) {
+        this.slsmo = slsmo;
+    }
+
+    public String getSlswk() {
+        return slswk;
+    }
+
+    public void setSlswk(String slswk) {
+        this.slswk = slswk;
+    }
+
+    public String getAppNum() {
+        return appNum;
+    }
+
+    public void setAppNum(String appNum) {
+        this.appNum = appNum;
+    }
+
+    public String getPoNum() {
+        return poNum;
+    }
+
+    public void setPoNum(String poNum) {
+        this.poNum = poNum;
+    }
+
+    public String getGuartran() {
+        return guartran;
+    }
+
+    public void setGuartran(String guartran) {
+        this.guartran = guartran;
+    }
+
+    public BigDecimal getNetSales() {
+        return netSales;
+    }
+
+    public void setNetSales(BigDecimal netSales) {
+        this.netSales = netSales;
+    }
+
+    public String getDebtorCode() {
+        return debtorCode;
+    }
+
+    public void setDebtorCode(String debtorCode) {
+        this.debtorCode = debtorCode;
+    }
+
+    public String getStrCode() {
+        return strCode;
+    }
+
+    public void setStrCode(String strCode) {
+        this.strCode = strCode;
+    }
+
+    public String getCoverage() {
+        return coverage;
+    }
+
+    public void setCoverage(String coverage) {
+        this.coverage = coverage;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getBranchNo() {
+        return branchNo;
+    }
+
+    public void setBranchNo(String branchNo) {
+        this.branchNo = branchNo;
+    }
+
+    public String getNaName() {
+        return naName;
+    }
+
+    public void setNaName(String naName) {
+        this.naName = naName;
+    }
+
+    public String getDsmName() {
+        return dsmName;
+    }
+
+    public void setDsmName(String dsmName) {
+        this.dsmName = dsmName;
+    }
+
+    public String getCoordinator() {
+        return coordinator;
+    }
+
+    public void setCoordinator(String coordinator) {
+        this.coordinator = coordinator;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+}
---
+++ src/main/java/ph/edu/up/antech/domain/sales/master/ZolMdcPerBranch.java
@@ -0,0 +1,252 @@
+package ph.edu.up.antech.domain.sales.master;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class ZolMdcPerBranch {
+
+    private LocalDate date;
+    private String customerCode;
+    private String customerName;
+    private String itemCode;
+    private String itemName;
+    private Integer salesUnit;
+    private BigDecimal salesValue;
+    private String antechProductDescription;
+    private BigDecimal antechPrice;
+    private BigDecimal amount;
+    private String account;
+    private String kam;
+    private String kamRefName;
+    private String stage;
+    private String amountConverted;
+    private String type;
+    private String loc;
+    private String cm;
+    private BigDecimal lessThan00375;
+    private BigDecimal v1;
+    private BigDecimal lessThan0853;
+    private BigDecimal v2;
+    private BigDecimal finalAmount;
+    private BigDecimal amountTimes1000;
+    private Integer a;
+    private String dsm;
+    private LocalDate cmInd;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getCustomerCode() {
+        return customerCode;
+    }
+
+    public void setCustomerCode(String customerCode) {
+        this.customerCode = customerCode;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public void setItemCode(String itemCode) {
+        this.itemCode = itemCode;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public Integer getSalesUnit() {
+        return salesUnit;
+    }
+
+    public void setSalesUnit(Integer salesUnit) {
+        this.salesUnit = salesUnit;
+    }
+
+    public BigDecimal getSalesValue() {
+        return salesValue;
+    }
+
+    public void setSalesValue(BigDecimal salesValue) {
+        this.salesValue = salesValue;
+    }
+
+    public String getAntechProductDescription() {
+        return antechProductDescription;
+    }
+
+    public void setAntechProductDescription(String antechProductDescription) {
+        this.antechProductDescription = antechProductDescription;
+    }
+
+    public BigDecimal getAntechPrice() {
+        return antechPrice;
+    }
+
+    public void setAntechPrice(BigDecimal antechPrice) {
+        this.antechPrice = antechPrice;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getKam() {
+        return kam;
+    }
+
+    public void setKam(String kam) {
+        this.kam = kam;
+    }
+
+    public String getKamRefName() {
+        return kamRefName;
+    }
+
+    public void setKamRefName(String kamRefName) {
+        this.kamRefName = kamRefName;
+    }
+
+    public String getStage() {
+        return stage;
+    }
+
+    public void setStage(String stage) {
+        this.stage = stage;
+    }
+
+    public String getAmountConverted() {
+        return amountConverted;
+    }
+
+    public void setAmountConverted(String amountConverted) {
+        this.amountConverted = amountConverted;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getLoc() {
+        return loc;
+    }
+
+    public void setLoc(String loc) {
+        this.loc = loc;
+    }
+
+    public String getCm() {
+        return cm;
+    }
+
+    public void setCm(String cm) {
+        this.cm = cm;
+    }
+
+    public BigDecimal getLessThan00375() {
+        return lessThan00375;
+    }
+
+    public void setLessThan00375(BigDecimal lessThan00375) {
+        this.lessThan00375 = lessThan00375;
+    }
+
+    public BigDecimal getV1() {
+        return v1;
+    }
+
+    public void setV1(BigDecimal v1) {
+        this.v1 = v1;
+    }
+
+    public BigDecimal getLessThan0853() {
+        return lessThan0853;
+    }
+
+    public void setLessThan0853(BigDecimal lessThan0853) {
+        this.lessThan0853 = lessThan0853;
+    }
+
+    public BigDecimal getV2() {
+        return v2;
+    }
+
+    public void setV2(BigDecimal v2) {
+        this.v2 = v2;
+    }
+
+    public BigDecimal getFinalAmount() {
+        return finalAmount;
+    }
+
+    public void setFinalAmount(BigDecimal finalAmount) {
+        this.finalAmount = finalAmount;
+    }
+
+    public BigDecimal getAmountTimes1000() {
+        return amountTimes1000;
+    }
+
+    public void setAmountTimes1000(BigDecimal amountTimes1000) {
+        this.amountTimes1000 = amountTimes1000;
+    }
+
+    public Integer getA() {
+        return a;
+    }
+
+    public void setA(Integer a) {
+        this.a = a;
+    }
+
+    public String getDsm() {
+        return dsm;
+    }
+
+    public void setDsm(String dsm) {
+        this.dsm = dsm;
+    }
+
+    public LocalDate getCmInd() {
+        return cmInd;
+    }
+
+    public void setCmInd(LocalDate cmInd) {
+        this.cmInd = cmInd;
+    }
+
+}
