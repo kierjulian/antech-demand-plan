56502500777f614309baf4d6899dca9288a58df3
Kier Julian Genoguin <kmgenoguin@up.edu.ph>
2020-02-08 19:02:04 +0800

    Added mdc per branch sales code page.
    Signed-off-by: Kier Julian Genoguin <kmgenoguin@up.edu.ph>

---
+++ src/main/java/ph/edu/up/antech/controller/config/MdcPerBranchSalesCodeController.java
@@ -0,0 +1,28 @@
+package ph.edu.up.antech.controller.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ph.edu.up.antech.domain.sales.master.converter.MdcPerBranchSalesCode;
+import ph.edu.up.antech.service.MdcPerBranchSalesCodeService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/config/mdc-branch-code")
+public class MdcPerBranchSalesCodeController {
+
+    @Autowired
+    private MdcPerBranchSalesCodeService mdcPerBranchSalesCodeService;
+
+    @GetMapping("")
+    public String loadMdcPerBranchSalesCodePage(Model model) {
+        List<MdcPerBranchSalesCode> mdcPerBranchSalesCodeList =
+                mdcPerBranchSalesCodeService.findAllMdcPerBranchSalesCode();
+        model.addAttribute("mdcPerBranchSalesCodeList", mdcPerBranchSalesCodeList);
+        return "mdc-branch-code";
+    }
+
+}
