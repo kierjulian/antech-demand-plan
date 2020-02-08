d98a80019677e922ca382f0c76546744be2b55d4
Kier Julian Genoguin <kmgenoguin@up.edu.ph>
2020-02-08 23:18:57 +0800

    Added netsuite other information table.
    Signed-off-by: Kier Julian Genoguin <kmgenoguin@up.edu.ph>

---
+++ src/main/java/ph/edu/up/antech/controller/config/NetsuiteOtherInformationController.java
@@ -0,0 +1,28 @@
+package ph.edu.up.antech.controller.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ph.edu.up.antech.domain.sales.master.converter.NetsuiteOtherInformation;
+import ph.edu.up.antech.service.NetsuiteOtherInformationService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/config/netsuite-other-info")
+public class NetsuiteOtherInformationController {
+
+    @Autowired
+    private NetsuiteOtherInformationService netsuiteOtherInformationService;
+
+    @GetMapping("")
+    public String loadNetsuiteOtherInformationPage(Model model) {
+        List<NetsuiteOtherInformation> netsuiteOtherInformationList =
+                netsuiteOtherInformationService.findAllNetsuiteOtherInformation();
+        model.addAttribute("netsuiteOtherInformationList", netsuiteOtherInformationList);
+        return "netsuite-other-info.html";
+    }
+
+}
