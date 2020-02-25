434d32359b22318f22543c58a444a256199d56b7
Kier Julian Genoguin <kmgenoguin@up.edu.ph>
2020-02-25 19:32:47 +0800

    Updated path for zol per doors.
    Signed-off-by: Kier Julian Genoguin <kmgenoguin@up.edu.ph>

--- src/main/java/ph/edu/up/antech/controller/view/master/ZolPerDoorsController.java
+++ src/main/java/ph/edu/up/antech/controller/view/master/ZolPerDoorsController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ph.edu.up.antech.domain.sales.master.ZolPerDoors;
 import ph.edu.up.antech.service.ZolPerDoorsService;
@@ -13,12 +14,13 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Controller
+@RequestMapping("/master/zol-doors")
 public class ZolPerDoorsController {
 
     @Autowired
     private ZolPerDoorsService zolPerDoorsService;
 
-    @GetMapping("/master/zol-per-doors")
+    @GetMapping("")
     public String loadZolPerDoorsMasterFile(Model model, @RequestParam(required = false) String date) {
         LocalDate localDate = !StringUtils.isNullOrEmpty(date) ? LocalDate.parse(date) : LocalDate.now();
         List<ZolPerDoors> zolPerDoorsList = zolPerDoorsService.findZolPerDoorsByDate(localDate);
