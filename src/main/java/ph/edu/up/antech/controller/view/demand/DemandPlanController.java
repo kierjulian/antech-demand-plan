7f28a86c7cb8009bdcb01c38973e0418f044a8f7
Kier Julian Genoguin <kmgenoguin@up.edu.ph>
2020-02-27 08:31:53 +0800

    Updated ui and path of demand plan. Refactored icon display in pages.
    Signed-off-by: Kier Julian Genoguin <kmgenoguin@up.edu.ph>

--- src/main/java/ph/edu/up/antech/controller/view/demand/DemandPlanController.java
+++ src/main/java/ph/edu/up/antech/controller/view/demand/DemandPlanController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/demand-plan")
+@RequestMapping("/demand/plan")
 public class DemandPlanController {
 
     private static final Logger LOGGER = Logger.getLogger(DemandPlanController.class);
