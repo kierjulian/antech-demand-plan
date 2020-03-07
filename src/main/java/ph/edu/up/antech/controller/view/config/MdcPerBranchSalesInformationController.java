2726c8b1b64f37677fbffab7186ef64b7a120d32
Kier Julian Genoguin <kmgenoguin@up.edu.ph>
2020-03-07 21:04:35 +0800

    Added pagination for mdc per branch sales information.
    Signed-off-by: Kier Julian Genoguin <kmgenoguin@up.edu.ph>

--- src/main/java/ph/edu/up/antech/controller/view/config/MdcPerBranchSalesInformationController.java
+++ src/main/java/ph/edu/up/antech/controller/view/config/MdcPerBranchSalesInformationController.java
@@ -2,14 +2,17 @@ package ph.edu.up.antech.controller.view.config;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import ph.edu.up.antech.dao.pagination.MdcPerBranchSalesInformationPaginationDAO;
 import ph.edu.up.antech.domain.sales.master.converter.MdcPerBranchSalesInformation;
 import ph.edu.up.antech.service.MdcPerBranchSalesInformationService;
-
-import java.util.List;
+import ph.edu.up.antech.util.StringUtils;
 
 @Controller
 @RequestMapping("/master/mdc-branch/config/info")
@@ -20,11 +23,17 @@ public class MdcPerBranchSalesInformationController {
     @Autowired
     private MdcPerBranchSalesInformationService mdcPerBranchSalesInformationService;
 
+    @Autowired
+    private MdcPerBranchSalesInformationPaginationDAO mdcPerBranchSalesInformationPaginationDAO;
+
     @GetMapping("")
-    public String loadMdcPerBranchSalesInformation(Model model) {
-        List<MdcPerBranchSalesInformation> mdcPerBranchSalesInformationList =
-                mdcPerBranchSalesInformationService.findAllMdcPerBranchSalesInformation();
-        model.addAttribute("mdcPerBranchSalesInformationList", mdcPerBranchSalesInformationList);
+    public String loadMdcPerBranchSalesInformation(Model model, @PageableDefault(size = 10) Pageable pageable,
+                                                   @RequestParam(required = false) String filter) {
+        Page<MdcPerBranchSalesInformation> page = StringUtils.isNullOrEmpty(filter)
+                ? mdcPerBranchSalesInformationPaginationDAO.findAll(pageable)
+                : mdcPerBranchSalesInformationPaginationDAO.findAllByAnyColumnContaining(filter, pageable);
+        model.addAttribute("page", page);
+        model.addAttribute("filter", filter);
         return "mdc-branch-info";
     }
 
---
+++ src/main/java/ph/edu/up/antech/dao/pagination/MdcPerBranchSalesInformationPaginationDAO.java
@@ -0,0 +1,16 @@
+package ph.edu.up.antech.dao.pagination;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import ph.edu.up.antech.domain.sales.master.converter.MdcPerBranchSalesInformation;
+
+public interface MdcPerBranchSalesInformationPaginationDAO extends PagingAndSortingRepository<MdcPerBranchSalesInformation, Integer> {
+
+    @Query("select o from MdcPerBranchSalesInformation o where o.itemNumber like %?1% or " +
+            "o.itemDescription like %?1% or o.itemShortDescription like %?1% or o.quantity like %?1% or " +
+            "o.price like %?1% or o.total like %?1% or o.itemCode like %?1% or o.stage like %?1% or o.itemType like %?1%")
+    public Page<MdcPerBranchSalesInformation> findAllByAnyColumnContaining(String filter, Pageable pageable);
+
+}
